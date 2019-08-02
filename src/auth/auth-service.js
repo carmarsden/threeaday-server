1994a8f2f883b255dbd460318557efc993d612b7
Caroline Marsden <car.marsden@gmail.com>
2019-08-02 17:24:49 -0400

    Add JWT authorization middleware

---
+++ src/auth/auth-service.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const AuthService = {
+    createJwt(subject, payload) {
+        return jwt.sign(payload, config.JWT_SECRET, {
+            subject,
+            expiresIn: config.JWT_EXPIRY,
+            algorithm: 'HS256'
+        })
+    },
+    verifyJwt(token) {
+        return jwt.verify(token, config.JWT_SECRET, {
+            algorithms: ['HS256'],
+        })
+    },
+    getUserWithUsername(db, user_name) {
+        return db('threeaday_users')
+            .where({ user_name })
+            .first()
+        ;
+    },
+}
+
+module.exports = AuthService;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://username@localhost/dbname',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/threeaday',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || true,
+    JWT_SECRET: process.env.JWT_SECRET || 'shhhhh',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '5h',
 }
   
