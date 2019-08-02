c0bf0e6eea951709da20f624f3ceaa8a6680120e
Caroline Marsden <car.marsden@gmail.com>
2019-08-02 12:17:41 -0400

    Update CORS parameters

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 const logger = require('./logger');
 const app = express();
 
@@ -15,7 +15,9 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}));
 
 
 // BASIC ENDPOINT
--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://username@localhost/dbname',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || true,
 }
   
